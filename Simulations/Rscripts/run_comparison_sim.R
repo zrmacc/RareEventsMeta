ba15c501f8a28ff25d0a8bdfd50935daa737c8b6
jlgrons <79216661+jlgrons@users.noreply.github.com>
2022-12-08 17:48:03 -0500

    Add results.

--- Simulations/Rscripts/run_comparison_sim.R
+++ Simulations/Rscripts/run_comparison_sim.R
@@ -5,32 +5,32 @@ setwd("/Users/jgrons/Documents/GitHub/RareEventsMeta/Simulations/Rscripts")
 
 ## Rare base rate, high heterogeneity, small n
 
-my_settings <- c(10, 3, 3, 0.002)
+my_settings <- c(40, 3, 3, 0.002)
 source("comparison_sim.R")
 
 ## Rare base rate, moderate heterogeneity, small n
 
-my_settings <- c(10, 8.5, 8.5, 0.002)
+my_settings <- c(40, 8.5, 8.5, 0.002)
 source("comparison_sim.R")
 
 ## Rare base rate, low heterogeneity, small n
 
-my_settings <- c(10, 20, 20, 0.002)
+my_settings <- c(40, 20, 20, 0.002)
 source("comparison_sim.R")
 
 ## Moderate base rate, high heterogeneity, small n
 
-my_settings <- c(10, 3, 3, 0.02)
+my_settings <- c(40, 3, 3, 0.02)
 source("comparison_sim.R")
 
 ## Moderate base rate, moderate heterogeneity, small n
 
-my_settings <- c(10, 8.5, 8.5, 0.02)
+my_settings <- c(40, 8.5, 8.5, 0.02)
 source("comparison_sim.R")
 
 ## Moderate base rate, low heterogeneity, small n
 
-my_settings <- c(10, 20, 20, 0.02)
+my_settings <- c(40, 20, 20, 0.02)
 source("comparison_sim.R")
 
 
@@ -40,30 +40,30 @@ source("comparison_sim.R")
 
 ## Rare base rate, high heterogeneity, small n
 
-my_settings <- c(10, 6, 3, 0.002)
+my_settings <- c(40, 6, 3, 0.002)
 source("comparison_sim.R")
 
 ## Rare base rate, moderate heterogeneity, small n
 
-my_settings <- c(10, 17, 8.5, 0.002)
+my_settings <- c(40, 17, 8.5, 0.002)
 source("comparison_sim.R")
 
 ## Rare base rate, low heterogeneity, small n
 
-my_settings <- c(10, 40, 20, 0.002)
+my_settings <- c(40, 40, 20, 0.002)
 source("comparison_sim.R")
 
 ## Moderate base rate, high heterogeneity, small n
 
-my_settings <- c(10, 6, 3, 0.02)
+my_settings <- c(40, 6, 3, 0.02)
 source("comparison_sim.R")
 
 ## Moderate base rate, moderate heterogeneity, small n
 
-my_settings <- c(10, 17, 8.5, 0.02)
+my_settings <- c(40, 17, 8.5, 0.02)
 source("comparison_sim.R")
 
 ## Moderate base rate, low heterogeneity, small n
 
-my_settings <- c(10, 40, 20, 0.02)
+my_settings <- c(40, 40, 20, 0.02)
 source("comparison_sim.R")
