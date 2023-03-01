e51e1681976060d79e42802ced5f3792eb1620b2
jlgrons <79216661+jlgrons@users.noreply.github.com>
2023-03-01 17:23:37 -0500

    Update files for Zack.

--- Simulations/Rscripts/check_results_analysis.R
+++ Simulations/Rscripts/check_results_analysis.R
@@ -39,4 +39,36 @@ all_dims/3
 # my_comp <- my_comp[, colSums(is.na(my_comp))==0]
 
 
+# Check the Type I error calculation for one setting.
+studies = 48
+alpha = 3; beta = 3
+rate = 0.015
+
+
+file_id <- paste0(
+  "~/Desktop/Comparison Simulation/Raw Results/Balanced/",
+  #"F:/School/MSc/STA4000/RareEventsMeta Simulations/Comparisons/Final Results 4 (ACTUAL FINAL RESULTS)/Balanced/",
+  "CP",
+  "_K", studies,
+  "_A", alpha,
+  "_B", beta,
+  "_R", rate,
+  ".rds"
+)
+my_data <- readRDS(file_id)
+my_comp <- my_data$all_comp
+my_res <- my_data$all_res
+
+all_comp <- my_data$all_comp
+all_rest <- my_data$all_res
+
+# Remove NA colums.
+all_comp_rm_NA <- all_comp[, colSums(is.na(all_comp))==0]
+dim(all_comp_rm_NA)
+comp_include_H0 <- rowMeans(all_comp_rm_NA[, seq(3, ncol(all_comp_rm_NA), by = 3)])
+type_1_error <- 1 - comp_include_H0
+type_1_error
+
+# This seems to match the plot here: https://drive.google.com/file/d/1gSg0oOVTGcjat4brc20rKb4z4j-KXE13/view?usp=sharing
+# The current results can be found in this folder: https://drive.google.com/drive/folders/1ftgW4TjavtV8uDEwfKWllmu67XIn95R_?usp=sharing
 
