29e03dcf0ed025b348bc0c7e17d250818be11c3f
jlgrons <79216661+jlgrons@users.noreply.github.com>
2023-03-08 08:50:12 -0500

    Update data_gen.R

--- RareEventsMeta/R/data_gen.R
+++ RareEventsMeta/R/data_gen.R
@@ -34,9 +34,13 @@ GenData <- function(
   y2 <- rpois(total_studies, n2 * base_rate)
 
   # Events in group 2. - I think this needs tot be alpha_1
+  # rr <- rbeta(total_studies, alpha2, beta2)
+  # y1 <- rpois(total_studies, n1 * base_rate * rr / (1 - rr))
+  beta2 <- alpha2 <- alpha1
   rr <- rbeta(total_studies, alpha2, beta2)
   y1 <- rpois(total_studies, n1 * base_rate * rr / (1 - rr))
   mean(base_rate * rr / (1 - rr))
+  mean(base_rate)
   # Data for analysis.
   data <- data.frame(
     "study" = seq_len(total_studies),
