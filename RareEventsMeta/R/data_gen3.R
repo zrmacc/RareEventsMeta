058f9259dc93cf94222752284bceb2a4a6a75cb2
jlgrons <79216661+jlgrons@users.noreply.github.com>
2023-03-08 07:57:01 -0500

    Add rnew data generation.

---
+++ RareEventsMeta/R/data_gen3.R
@@ -0,0 +1,53 @@
+# -----------------------------------------------------------------------------
+# Data Generation
+# -----------------------------------------------------------------------------
+
+#' Generate Data
+#'
+#' @param total_studies Total number of studies.
+#' @param n1 Sample size in group 1.
+#' @param n2 Sample size in group 2.
+#' @param alpha1 Shape parameters for group 1.
+#' @param rate1 Baseline event rate in group 1.
+#' @param alpha2 First shape parameter for beta distribution,
+#'   lambda_1/(lambda_1+lambda_2) ~ B(alpha2, beta2).
+#' @param beta2 Second shape parameter for beta distribution,
+#'   lambda_1/(lambda_1+lambda_2) ~ B(alpha2, beta2).
+#'
+#' @importFrom stats rbeta rgamma rpois
+#' @export
+
+GenData <- function(
+  total_studies,
+  n1,
+  n2,
+  beta,
+  psi,
+  gamma,
+  null = FALSE
+) {
+
+
+  base_rate <- rgamma(total_studies, 1.44, beta)
+  y2 <- rpois(total_studies, n2 * base_rate)
+
+  if(!null){
+
+  tau <- log(n2/n1) + logit(rbeta(total_studies, psi * gamma, psi))
+
+  }else{tau = 0}
+
+  y1 <- rpois(total_studies, n1 * base_rate * exp(tau))
+
+  # Data for analysis.
+  data <- data.frame(
+    "study" = seq_len(total_studies),
+    "size_1" = n1,
+    "events_1" = y1,
+    "size_2" = n2,
+    "events_2" = y2
+  )
+
+  return(data)
+}
+
